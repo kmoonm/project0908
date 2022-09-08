6f62fe812cb959d855662233db933d7fe49c2c36
yj-603-16 <yj-603-16@DESKTOP-UF2GQHS>
2022-09-08 10:23:33 +0900

    project0908 두번째

---
+++ src/main/java/d0908/exam00.java
@@ -0,0 +1,12 @@
+package d0908;
+
+public class exam00 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("헬로월드");
+		// TODO Auto-generated method stub
+
+	}
+
+}
